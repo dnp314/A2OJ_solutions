b2b933018ebcfff74c61cb04a71429032585863d
dnp314 <119059430+dnp314@users.noreply.github.com>
2023-06-23 19:51:43 +0530

    Add files via upload

---
+++ Word_Capitalization.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://codeforces.com/contest/281/problem/A
+
+int main()
+{
+string x;
+cin>>x;
+if(x[0]>96)
+x[0]=x[0]-32;
+cout<<x;
+}
+
+
+
+
+
+
+
